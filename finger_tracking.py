31788df188be7c101f737b2dfd871dc9778c0a47
KushKumarK <91371411+KushKumarK@users.noreply.github.com>
2023-03-09 10:30:19 +0530

    TrackingHands

--- finger_tracking.py
+++ finger_tracking.py
@@ -1,19 +1,55 @@
 import cv2
 import mediapipe as mp
-
-#initializing cv2
+import serial
+#initializing cv2 and Serial
 cap = cv2.VideoCapture(0)
+#ser = serial.Serial(port="COM12",baudrate=115200)
 
 #initializing mediapipe utilities
 mp_draw = mp.solutions.drawing_utils
 mp_draw_style = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.8, min_tracking_confidence=0.8) as hand:
+with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.9, min_tracking_confidence=0.9, max_num_hands=1) as hand:
   while cap.isOpened():
     success, img = cap.read()
     if not success:
       #Empty camera frame
       print("Empty Camera Frame")
       continue
-     
+    #Get the window dimensions
+    h,w,c=img.shape
+    img.flags.writeable = False
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hand.process(img)
+    
+    
+    img.flags.writeable = True
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # getting the landmark list and drawing it
+    lmList=[]
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_draw.draw_landmarks(
+        img,
+        hand_landmarks,
+        mp_hands.HAND_CONNECTIONS,
+        mp_draw_style.get_default_hand_landmarks_style(),
+        mp_draw_style.get_default_hand_connections_style())
+      myHand=results.multi_hand_landmarks[0]
+      for id,lm in enumerate(myHand.landmark):
+        cx,cy = int(lm.x*w),int(lm.y*h)
+        lmList.append([id,cx,cy])
+      cv2.putText(img,str(1), (lmList[4][1],lmList[4][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
+      cv2.putText(img,str(2), (lmList[8][1],lmList[8][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
+      cv2.putText(img,str(3), (lmList[12][1],lmList[12][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
+      cv2.putText(img,str(4), (lmList[16][1],lmList[16][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
+      cv2.putText(img,str(5), (lmList[20][1],lmList[20][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
+      
+    img = cv2.flip(img, 1)
+    cv2.imshow('MediaPipe Hands', cv2.flip(img, 1))
+    key = cv2.waitKey(1)
+    if key==ord("q"):
+      print("done")
+      break
+cap.release()
