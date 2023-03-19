89b5b8cec0b2901e061e5ab190a73bdda28ece20
KushKumarK <91371411+KushKumarK@users.noreply.github.com>
2023-03-19 08:25:37 +0530

    tracking Y axis

--- finger_tracking.py
+++ finger_tracking.py
@@ -7,11 +7,11 @@ import serial
 cap = cv2.VideoCapture(0)
 #ser = serial.Serial(port="COM12",baudrate=115200)
 val = 1
-dist_str = ""
+distYf = []
 isHandDetected = 0
 #serial communication function
 def serialComm():
-  global val, dist_str, isHandDetected
+  global val, distYf, isHandDetected
   while True:
     if val == 1:
       print("done")
@@ -26,7 +26,7 @@ mp_draw_style = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 def TrackHand():
-  global dist_str,val,isHandDetected
+  global distYf,val,isHandDetected
   with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.9, min_tracking_confidence=0.9, max_num_hands=1) as hand:
     while cap.isOpened():
       success, img = cap.read()
@@ -55,16 +55,20 @@ def TrackHand():
         for id,lm in enumerate(myHand.landmark):
           cx,cy = int(lm.x*w),int(lm.y*h)
           lmList.append([id,cx,cy])
-        for i in range(4,21,4):
-            str1 = "{"+str(lmList[i][1]) + "," + str(lmList[i][2])+"}"
-            dist_str+=str1
-        print(dist_str)    
+        distY = lmList[0][2] - lmList[5][2]
+        distYf = [lmList[4][2],lmList[8][2], lmList[12][2],lmList[16][2], lmList[20][2]]    
         cv2.putText(img,str(1), (lmList[4][1],lmList[4][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
         cv2.putText(img,str(2), (lmList[8][1],lmList[8][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
         cv2.putText(img,str(3), (lmList[12][1],lmList[12][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
         cv2.putText(img,str(4), (lmList[16][1],lmList[16][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
         cv2.putText(img,str(5), (lmList[20][1],lmList[20][2]), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
-
+        cv2.line(img,(lmList[0][1],lmList[0][2]),(lmList[5][1],lmList[5][2]),(255,255,255),4)
+        cv2.circle(img, ((lmList[0][1] + lmList[5][1])//2, (lmList[0][2] + lmList[5][2])//2), 0,(0,0,0),8)
+        cv2.putText(img,str(distY), ((lmList[0][1] + lmList[5][1])//2, (lmList[0][2] + lmList[5][2])//2), cv2.FONT_HERSHEY_SIMPLEX,1,(209,80,0,255),3)
+        if distY>0:
+          isHandDetected = 1
+        else:
+          isHandDetected = 0
       img = cv2.flip(img, 1)
       cv2.imshow('MediaPipe Hands', cv2.flip(img, 1))
       key = cv2.waitKey(1)
